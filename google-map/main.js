657129e8bf5f1569379cd9ef98eef7eda854d481
Ruslan Lozhkin <18569837+RuslanLozhkin@users.noreply.github.com>
2019-12-09 19:01:49 +0200

    try to add marker to the google map

--- google-map/main.js
+++ google-map/main.js
@@ -171,7 +171,7 @@ initMap = function() {
   });
 
   var image = new google.maps.MarkerImage(
-    "../images/BA_Logo_PNG-transparency-1.png",
+    "../images/marker.png",
     new google.maps.Size(130, 130),
     new google.maps.Point(0, 0),
     new google.maps.Point(22, 32)
